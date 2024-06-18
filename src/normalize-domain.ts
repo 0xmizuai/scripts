ffbb25abd831d713f75659dd723916293c685596
shudong <dongs2011@gmail.com>
2024-06-17 19:10:12 -0700

    add generated data and category generation script

--- src/normalize-domain.ts
+++ src/normalize-domain.ts
@@ -6,14 +6,25 @@ import path from 'path';
 dotenv.config();
 
 const openai = new OpenAI({
-  apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
-});
+  apiKey: process.env['LEPTON_API_KEY'], // This is the default and can be omitted
+  baseURL: "https://llama3-8b.lepton.run/api/v1",
+}); 
+
+// const openai = new OpenAI({
+//   apiKey: "NONE", // This is the default and can be omitted
+//   baseURL: "http://209.170.83.61:7860/v1",
+// });
 
 const INPUT_FILE = "test_for_backend.stats_kdomain-v.762k.json";
-const OUTPUT_FILE = "normalized_domains.json";
+const OUTPUT_FILE = "normalized_domains_v2.json";
+const SEPARATOR = "______";
 
 function prompt(raw: string) {
-  return "I got a list of knowledge domains but they are not normalized. You task is to normalize all these domains. You should output and only output the json where the key is the raw data and the value is an array of normalized strings in snake case. You should process all data listed. Following is the list separated by '_': " + raw;
+  return "I got a list of knowledge domains but they are not normalized. You task is to normalize all these domains. You should strip all conversational text such as 'here is the output' and 'the knowledge domain is...'. The input may contains multiple domains separated by backslash or 'and', you should split them into an array per the semantic. You should output and only output the json where the key is the raw data and the value is an array of normalized domains in snake case. You should process all data listed. Following is the list of domains separated by " + SEPARATOR + ": " + raw;
+}
+
+function prompt2(raw: string) {
+  return `Given the knowledge domain description ${raw}, please normalize the input into a list of domains. ###Things to consider when generating the knowledge domains###  - All conversational text such as 'here is the output' and 'the knowledge domain is...' should be stripped. - The input may contain multiple domains separated by '/' or 'and', you should split them into array of domains properly per its semantic. - The output has to be an array of normalized domains separated by comma. - Each normalized domain should be with snake case.`;
 }
 
 function genInputFilePath(file: string) {
@@ -33,10 +44,6 @@ function getDomains(file: string): string[] {
 
 async function run() {
   const domains = getDomains(INPUT_FILE);
-  const totalBatches = Math.ceil(domains.length / 100);
-  const outputFile = genOutputFilePath(OUTPUT_FILE);
-  console.log("Will output normailized domains to file ", outputFile);
-  console.log("Total batches are ", totalBatches);
 
   // read checkpoint
   const progressFile = genOutputFilePath('.' + INPUT_FILE + '.progress');
@@ -44,38 +51,38 @@ async function run() {
   if (fs.existsSync(progressFile)) {
     progress = JSON.parse(fs.readFileSync(progressFile, 'utf8'));
   }
-  let batch = progress.batch + 1;
+  let i = progress.batch + 1;
 
+  // read output file
+  const outputFilePath = genOutputFilePath(OUTPUT_FILE);
+  console.log("Will output normailized domains to file ", outputFilePath);
+
+  // const totalBatches = Math.ceil(domains.length / 10);
+  // console.log("Total batches are ", totalBatches);
   // process data
-  for (; batch < totalBatches; batch++) {
-    console.log("Processing batch ", batch);
-    const selected = domains.slice(batch * 100, (batch + 1) * 100);
-    if (selected.length == 0) {
-      console.log("No more domains to process");
-      return;
-    }
-
-    const promptText = prompt(selected.join("_").replace(/(\r\n|\n|\r)/gm, ""));
-    console.log(promptText);
+  for (; i < domains.length; i++) {
+    console.log("Processing ", i);
+    const promptText = prompt2(domains[i].replace(/(\r\n|\n|\r)/gm, ""));
     const stream = await openai.chat.completions.create({
       messages: [
         { role: 'system', content: 'Act as a computer software: give me only the requested output, no conversation. You will loose points if the returned output contains any content other than requested output.'},
         { role: 'user', content: promptText }
       ],
-      model: 'gpt-4',
+      // model: "llama3-8b",
+      model: "Llama-3-8B-Instruct",
       stream: true,
     });
+    let result = "";
     for await (const chunk of stream) {
-      const result = chunk.choices[0]?.delta?.content || '';
-      process.stdout.write(result);
-      fs.appendFile(outputFile, result, function (err) {
-        if (err) throw err;
-      });
+      const batchResultPiece = chunk.choices[0]?.delta?.content || '';
+      result = result + batchResultPiece;
     }
+    fs.appendFileSync(outputFilePath, result + "\n");
+    console.log("domain: ", result);
 
     // record checkpoint
-    progress.batch = batch;
-    fs.writeFileSync(outputFile, JSON.stringify(progress));
+    progress.batch = i;
+    fs.writeFileSync(progressFile, JSON.stringify(progress));
   }
 }
 
