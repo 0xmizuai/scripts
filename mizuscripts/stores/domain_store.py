dacd1e2beee1eba0ea09c1f268c17767b850d389
GuannanMa <s4001212@student.rmit.edu.au>
2024-07-09 22:39:58 +0800

    fix edge case

--- mizuscripts/stores/domain_store.py
+++ mizuscripts/stores/domain_store.py
@@ -17,12 +17,16 @@ class DomainStore():
         self.documents = "\n\n".join([domain["name"] for domain in collection.find()])
         self.store = self.vector_store()
     
-    def vector_store(self) -> Chroma:
+    def vector_store(self) -> Optional[Chroma]:
         splitter = RecursiveCharacterTextSplitter(chunk_size=1, chunk_overlap=0, separators=["\n\n"], keep_separator=False)
+        if len(self.documents) == 0:
+            return None
         documents = splitter.create_documents([self.documents])
         return Chroma.from_documents(splitter.split_documents(documents), OpenAIEmbeddings()).as_retriever(search_type="similarity_score_threshold", search_kwargs={"score_threshold": DomainStore.THRESHOLD})
     
     def search(self, domain: str) -> Optional[str]:
+        if self.store is None:
+            return None
         documents = self.store.invoke(f"{domain}") 
         if len(documents) == 0:
             return None
